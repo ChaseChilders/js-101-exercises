a564fa6e60ea21d087a01541a5c4a0aa4ab1df95
Chase Childers <chasechilders12@gmail.com>
2022-04-22 16:22:42 -0400

    finished exercises lvl 3

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,31 @@
-function lvl3exercise1 () {
+function lvl3exercise1() {
   // Create strings "hello" and a "world", return the concatenation of the two
-
+  myString = "hello";
+  myString2 = "world";
+  return myString + myString2;
 }
 
-function lvl3exercise2 () {
+function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  myString = "hello";
+  myString2 = 12;
+  return myString + myString2;
 }
 
-function lvl3exercise3 () {
+function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  myVariable = 12;
+  return 12 + "";
 }
 
-function lvl3exercise4 () {
+function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
-
+  string = "hello world!";
+  return string.length;
 }
 
-function lvl3exercise5 () {
+function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  string = "Hello World";
+  return string.indexOf("World");
 }
